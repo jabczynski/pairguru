f16c877cabcbcb39421ed734ab9807b4333cfdc8
Marek Jabczyński <marjab92@gmail.com>
2019-06-20 16:51:39 +0200

    Send email asynchroniously. Export movies asynchroniously

--- spec/requests/movies_spec.rb
+++ spec/requests/movies_spec.rb
@@ -7,4 +7,17 @@ describe "Movies requests", type: :request do
       expect(page).to have_selector("h1", text: "Movies")
     end
   end
+
+  describe "send info" do
+    let(:user) { create :user }
+    let(:movie) { create :movie, title: "Fake title" }
+
+    it "redirects back to previous page" do
+      sign_in(user)
+      visit "/movies/#{movie.id}"
+      click_link "Email me details about this movie"
+      expect(page).to have_selector("#flash_notice", text: "Email sent with movie info")
+      expect(page).to have_selector("h1", text: "Fake title")
+    end
+  end
 end
